5461a0a07bec6307f96bec94e01267f84c5d8bef
Btadaboina-loyaltymethods <141708084+Btadaboina-loyaltymethods@users.noreply.github.com>
2023-08-25 16:59:52 +0530

    pagination with dom and update servings

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -10,6 +10,32 @@ export default class View {
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
+
+  update(data) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+
+    this._data = data;
+    const newmarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newmarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    // console.log(newElements);
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      console.log(curEl, newEl.isEqualNode(curEl));
+
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild.nodeValue.trim() !== ''
+      ) {
+        console.log('HEllo', newEl.firstChild.nodeValue.trim());
+        curEl.textContent = newEl.textContent;
+      }
+    });
+  }
+
   _clear() {
     this._parentElement.innerHTML = '';
   }
