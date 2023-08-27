75238fb91d9dc2f8e474d77701254179ec5aa6c6
Btadaboina-loyaltymethods <141708084+Btadaboina-loyaltymethods@users.noreply.github.com>
2023-08-27 23:25:48 +0530

    Update view.js

--- view.js
+++ view.js
@@ -1,45 +1,41 @@
-import View from './View.js';
-import icons from 'url:../../img/icons.svg'; // Parcel 2
+import View from './view';
+// import icons from 'url:../../img/icons.svg';
+// // import math
+// class AddRecipeView extends View {
+//   _parentElement = document.querySelector('.upload');
+//   _window = document.querySelector('.add-recipe-window');
+//   _overlay = document.querySelector('.overlay');
+//   _btnOpen = document.querySelector('.nav__btn--add-recipe');
+//   _btnClose = document.querySelector('.btn--close-modal');
 
-class AddRecipeView extends View {
-  _parentElement = document.querySelector('.upload');
-  _message = 'Recipe was successfully uploaded :)';
+//   constructor() {
+//     super();
+//     this._addHandlerShowWindow();
+//     this._addHandlerHideWindow();
+//   }
 
-  _window = document.querySelector('.add-recipe-window');
-  _overlay = document.querySelector('.overlay');
-  _btnOpen = document.querySelector('.nav__btn--add-recipe');
-  _btnClose = document.querySelector('.btn--close-modal');
+//   toggleWindow() {
+//     this._overlay.classList.toggle('hidden');
+//     this.window.classList.toggle('hidden');
+//   }
 
-  constructor() {
-    super();
-    this._addHandlerShowWindow();
-    this._addHandlerHideWindow();
-  }
+//   _addHandlerShowWindow() {
+//     this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+//   }
+//   _addHandlerHideWindow() {
+//     this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+//     this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+//   }
+//   addHandlerUpload(handler) {
+//     this._parentElement.addEventListener('sumbit', function (e) {
+//       e.preventDefault();
+//       const data = [...new FormData(this)];
+//       console.log(data);
+//       handler(data);
+//     });
+//   }
 
-  toggleWindow() {
-    this._overlay.classList.toggle('hidden');
-    this._window.classList.toggle('hidden');
-  }
+//   _generateMarkup() {}
+// }
 
-  _addHandlerShowWindow() {
-    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
-  }
-
-  _addHandlerHideWindow() {
-    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
-    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
-  }
-
-  addHandlerUpload(handler) {
-    this._parentElement.addEventListener('submit', function (e) {
-      e.preventDefault();
-      const dataArr = [...new FormData(this)];
-      const data = Object.fromEntries(dataArr);
-      handler(data);
-    });
-  }
-
-  _generateMarkup() {}
-}
-
-export default new AddRecipeView();
+// export default new AddRecipeView();
